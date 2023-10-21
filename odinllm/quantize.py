8211230723e2c1e707943b28d7876b90691e65db
Prof. Peter Schneider-Kamp <jps@ordbogen.com>
2023-10-21 09:17:10 +0200

    output for quantize

--- odinllm/quantize.py
+++ odinllm/quantize.py
@@ -1,7 +1,7 @@
 import click
 from transformers import AutoModelForCausalLM, AutoTokenizer, GPTQConfig
 
-from .utils import Namespace, parse_device_map
+from .utils import Namespace, end, parse_device_map, start
 
 @click.group()
 def _quantize():
@@ -19,8 +19,12 @@ def quantize(**kwargs):
     args.device_map = parse_device_map(args.device_map)
     args.bits = int(args.bits)
     args.group_size = int(args.group_size)
-    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model, use_fast=True)
 
+    start("Loading tokenizer from", args.pretrained_model)
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model, use_fast=True)
+    end()
+
+    start("Loading and quantizing model from", args.pretrained_model, "to", args.bits," bits with group size", args.group_size, f"and {'' if args.act_order else 'no'} act order using dataset", args.dataset)
     quantization_config = GPTQConfig(
         bits=args.bits,  # quantize model to 4-bit
         group_size=args.group_size,  # it is recommended to set the value to 128
@@ -28,16 +32,16 @@ def quantize(**kwargs):
         dataset = args.dataset,
         tokenizer = tokenizer,
     )
-
-    # load un-quantized model, by default, the model will always be loaded into CPU memory
     model = AutoModelForCausalLM.from_pretrained(args.pretrained_model, quantization_config=quantization_config, device_map=args.device_map)
+    end())
 
-    # quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask"
-    #model.quantize(examples)
-
-    # save quantized model
+    start("Saving quantized model to", args.quantized_model)
     model.save_pretrained(args.quantized_model)
+    end()
+
+    start("Saving tokenizer to", args.quantized_model)
     tokenizer.save_pretrained(args.quantized_model)
+    end()
 
 if __name__ == "__main__":
     quantize()
