cbb3917288bc44ffa7bdd5fb58697691e67fd2de
Prof. Peter Schneider-Kamp <jps@ordbogen.com>
2023-10-21 09:20:38 +0200

    fixed typos

--- odinllm/quantize.py
+++ odinllm/quantize.py
@@ -24,7 +24,7 @@ def quantize(**kwargs):
     tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model, use_fast=True)
     end()
 
-    start("Loading and quantizing model from", args.pretrained_model, "to", args.bits," bits with group size", args.group_size, f"and {'' if args.act_order else 'no'} act order using dataset", args.dataset)
+    start("Loading and quantizing model from", args.pretrained_model, "to", args.bits, "bits with group size", args.group_size, f"and {'' if args.act_order else 'no'} act order using dataset", args.dataset)
     quantization_config = GPTQConfig(
         bits=args.bits,  # quantize model to 4-bit
         group_size=args.group_size,  # it is recommended to set the value to 128
@@ -33,7 +33,7 @@ def quantize(**kwargs):
         tokenizer = tokenizer,
     )
     model = AutoModelForCausalLM.from_pretrained(args.pretrained_model, quantization_config=quantization_config, device_map=args.device_map)
-    end())
+    end()
 
     start("Saving quantized model to", args.quantized_model)
     model.save_pretrained(args.quantized_model)
