4bf2ea240a05f31bc3acd0a44f9a8491e286e796
Prof. Peter Schneider-Kamp <jps@ordbogen.com>
2023-10-22 09:01:47 +0200

    moving things to shared

--- odinllm/infer.py
+++ odinllm/infer.py
@@ -1,13 +1,7 @@
 import click
-from peft import PeftModel
-from transformers import AutoModelForCausalLM, AutoTokenizer, GPTQConfig
-import logging
 
-from .utils import EXAMPLE_PROMPTS, end, parse_args, start, status
-
-logging.basicConfig(
-    format="%(asctime)s %(levelname)s [%(name)s] %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S"
-)
+from .shared import load_tokenizer, load_model, load_lora, run_prompt
+from .utils import parse_args
 
 @click.group()
 def _infer():
@@ -15,35 +9,17 @@ def _infer():
 @_infer.command()
 @click.argument("pretrained-model", type=click.Path(exists=True))
 @click.option("--lora-adapter", "-l", default=None, help="Optional LoRA adapter to load with PEFT")
-@click.option("--eval-prompt", "-p", default=None, help="Prompt to run instead of example prompts")
+@click.option("--run-prompt", "-p", default=None, help="Prompt to run instead of example prompts")
 @click.option("--device-map", "-m", default="auto")
 @parse_args
 def infer(args):
-
-    start("Loading tokenizer from", args.pretrained_model)
-    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model, use_fast=True)
-    end()
-
-    start("Loading pretrained model from", args.pretrained_model)
-    model = AutoModelForCausalLM.from_pretrained(args.pretrained_model, device_map=args.device_map)
-    end()
+    tokenizer = load_tokenizer(args.pretrained_model)
+    model = load_model(args.pretrained_model, device_map=args.device_map)
 
     if args.lora_adapter is not None:
-        start("Loading LoRA adapter from", args.lora_adapter)
-        model = PeftModel.from_pretrained(model, args.lora_adapter)
-        end()
+        model = load_lora(model, args.lora_adapter)
 
-    if args.eval_prompt is not None:
-        start("Testing given prompt", args.eval_prompt)
-        res = tokenizer.decode(model.generate(**tokenizer(args.eval_prompt, return_tensors="pt").to(model.device),max_new_tokens=128)[0])
-        end(end='')
-        status(res)
-    else:
-        for key, prompt in EXAMPLE_PROMPTS.items():
-            start(f"Testing {key} prompt")
-            res = tokenizer.decode(model.generate(**tokenizer(prompt, return_tensors="pt").to(model.device),max_new_tokens=256)[0])
-            end(end='')
-            status(res)
+    print(run_prompt(model, tokenizer, args.run_prompt))
 
 if __name__ == "__main__":
     infer()
