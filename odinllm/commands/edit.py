713b450dcb15142a3f47a11baca234dbaa714e93
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2024-02-06 15:37:05 +0100

    correct continuous tokenizer trainer

--- odinllm/commands/edit.py
+++ odinllm/commands/edit.py
@@ -11,11 +11,51 @@ from transformers.models.mixtral.modeling_mixtral import MixtralAttention, Mixtr
 from ..shared import load_model, load_tokenizer, save_metadata, save_model, save_tokenizer
 from ..utils import args_config, end, format_text, start, status, trainable_parameters
 
-def tprint(tokens, decode):
-    pass#print([(k, decode[k]) for k in tokens])
+#def tprint(tokens, decode):
+#    print([(decode[k] if k >= 0 else '<DEL>') for k in tokens])
 
-def nprint(ngrams, decode):
-    print({f"{decode[a]} {decode[b]}": c for (a, b), c in ngrams.items()})
+#def nprint(ngrams, decode):
+#    print({f"{decode[a]} {decode[b]}": c for (a, b), c in ngrams.items()})
+
+#def cprint(num2ngrams, decode):
+#    print({k: [f"{decode[a]} {decode[b]}" for a, b in v] for k, v in num2ngrams.items()})
+
+#def check_ngrams(ngrams, num2ngrams):
+#    for ngram, num in ngrams.items():
+#        assert ngram in num2ngrams[num]
+#    for num, ns in num2ngrams.items():
+#        for n in ns:
+#            assert ngrams[n] == num
+
+def add_ngram(ngrams, num2ngrams, a, b, decode):
+    #print(f"Adding {decode[a]} {decode[b]}")
+    #check_ngrams(ngrams, num2ngrams)
+    ngram = (a, b)
+    num = ngrams[ngram]+1
+    ngrams[ngram] = num
+    num2ngrams[num][ngram] = None
+    if num-1:
+        del num2ngrams[num-1][ngram]
+    #nprint(ngrams, decode)
+    #cprint(num2ngrams, decode)
+    #check_ngrams(ngrams, num2ngrams)
+    return num, ngram
+
+def del_ngram(ngrams, num2ngrams, a, b, decode):
+    #print(f"Deleting {decode[a]} {decode[b]}")
+    #check_ngrams(ngrams, num2ngrams)
+    ngram = (a, b)
+    num = ngrams[ngram]-1
+    if num:
+        ngrams[ngram] = num
+        num2ngrams[num][ngram] = None
+    else:
+        del ngrams[ngram]
+    del num2ngrams[num+1][ngram]
+    #nprint(ngrams, decode)
+    #cprint(num2ngrams, decode)
+    #check_ngrams(ngrams, num2ngrams)
+    return num, ngram
 
 @click.group()
 def _edit():
@@ -187,31 +227,32 @@ def __edit(args, config):
         for samples in tqdm(corpora, desc="Tokenizing corpora"):
             for sample in tqdm(samples, desc="Tokenizing samples"):
                 tokenized.extend(tokenizer(sample["text"])["input_ids"])
-        tprint(tokenized, decode)
+        #tprint(tokenized, decode)
         end()
         start(f"Extending vocabulary from {model.config.vocab_size} to {model.config.vocab_size+args.extend_vocab}")
+        tokenizer_json = json.load(open(os.path.join(args.pretrained_model, "tokenizer.json"), "rt"))
+        special_tokens = {t["id"]: t["content"] for t in tokenizer_json["added_tokens"]}
         ngrams = defaultdict(int)
-        token2indices = defaultdict(list)
-        num2ngrams = defaultdict(set)
+        token2indices = defaultdict(dict)
+        num2ngrams = defaultdict(dict)
         max_ngram = 0
         max_in_word_ngram = 0
-        for i in tqdm(range(len(tokenized)-1), desc="Creating ngram statistics"):
-            ngram = (tokenized[i], tokenized[i+1])
-            num = ngrams[ngram]+1
-            ngrams[ngram] = num
-            num2ngrams[num].add(ngram)
-            if num > max_ngram:
-                max_ngram = num
-            if num > max_in_word_ngram and decode[ngram[1]][0].isalpha():
-                max_in_word_ngram = num
-            if num-1:
-                num2ngrams[num-1].remove(ngram)
-            token2indices[tokenized[i]].append(i)
+        len_tokenized = len(tokenized)
+        for i in tqdm(range(len_tokenized), desc="Creating ngram statistics"):
+            if tokenized[i] in special_tokens:
+                continue
+            if i+1 < len_tokenized and not tokenized[i+1] in special_tokens:
+                num, ngram = add_ngram(ngrams, num2ngrams, tokenized[i], tokenized[i+1])
+                if num > max_ngram:
+                    max_ngram = num
+                if num > max_in_word_ngram and decode[ngram[1]][0].isalpha():
+                    max_in_word_ngram = num
+            token2indices[tokenized[i]][i] = None
         #nprint(ngrams, decode)
-        min_in_word_bigrams = 1 if args.min_in_word_bigrams is None else args.min_in_word_bigrams if args.min_in_word_bigrams else max_in_word_ngram+1
-        min_bigrams = 1 if args.min_bigrams is None else args.min_bigrams if args.min_bigrams else max_ngram+1
+        min_in_word_bigrams = 1 if args.min_in_word_bigrams is None else args.min_in_word_bigrams if args.min_in_word_bigrams >= 0 else max_in_word_ngram+1
+        min_bigrams = 1 if args.min_bigrams is None else args.min_bigrams if args.min_bigrams >= 0 else max_ngram+1
         extra_merges = []
-        pbar = tqdm(total=args.extend_vocab, desc="Extending vocabulary")
+        pbar = tqdm(total=args.extend_vocab, desc="Extending vocabulary", disable=False)
         while len(vocab) < max_vocab_len:
             #status(f"{next_id}", end='')
             best_pair = None
@@ -239,10 +280,13 @@ def __edit(args, config):
             #end(end='')
             #status(f"{num} out of {max_in_word_ngram} out of {max_ngram}", end='')
             #nprint({best_pair: ngrams[best_pair]}, decode)
+            #nprint(ngrams, decode)
+            #cprint(num2ngrams, decode)
+            #tprint(tokenized, decode)
             #end(end='')
             num = ngrams[best_pair]
             del ngrams[best_pair]
-            num2ngrams[num].remove(best_pair)
+            del num2ngrams[num][best_pair]
             a, b = decode[best_pair[0]], decode[best_pair[1]]
             extra_merges.append(f"{a} {b}")
             next_token = f"{a}{b}"
@@ -250,7 +294,9 @@ def __edit(args, config):
             decode[next_id] = next_token
             #end(end='')
             len_tokenized = len(tokenized)
-            for i in token2indices[a].copy():
+            a, b = best_pair
+            for i in list(token2indices[a].keys()):
+                #print(token2indices)
                 _a = tokenized[i]
                 if _a == -1:
                     continue
@@ -259,48 +305,48 @@ def __edit(args, config):
                 j = i+1
                 while j < len_tokenized:
                     _b = tokenized[j]
-                    if _b != -1:
+                    if _b >= 2:
                         break
                     j += 1
                 if _b != b:
                     continue
+                #print(i,j)
                 tokenized[i] = next_id
                 tokenized[j] = -1
-                token2indices[a].remove(i)
-                token2indices[b].remove(j)
-                token2indices[next_id].append(i)
+                del token2indices[a][i]
+                del token2indices[b][j]
+                token2indices[next_id][i] = None
                 _i = i-1
                 while _i >= 0:
                     if tokenized[_i] != -1:
-                        ngram = (tokenized[_i], next_id)
-                        num = ngrams[ngram]+1
-                        ngrams[ngram] = num
-                        num2ngrams[num].add(ngram)
-                        if num-1:
-                            num2ngrams[num-1].remove(ngram)
+                        if tokenized[_i] in special_tokens:
+                            break
+                        add_ngram(ngrams, num2ngrams, tokenized[_i], next_id)
+                        del_ngram(ngrams, num2ngrams, tokenized[_i], a)
                         break
                     _i -= 1
                 _j = j+1
                 while _j < len_tokenized:
                     if tokenized[_j] != -1:
-                        ngram = (next_id, tokenized[_j])
-                        num = ngrams[ngram]+1
-                        ngrams[ngram] = num
-                        num2ngrams[num].add(ngram)
-                        if num-1:
-                            num2ngrams[num-1].remove(ngram)
+                        if tokenized[_j] in special_tokens:
+                            break
+                        add_ngram(ngrams, num2ngrams, next_id, tokenized[_j])
+                        del_ngram(ngrams, num2ngrams, b, tokenized[_j])
                         break
                     _j += 1
             #end(end='')
-            if not next_id % 10000:
-                tokenized = [t for t in tokenized if t != -1]
             next_id += 1
+            if False or not next_id % 1000:
+                tokenized = [t for t in tqdm(tokenized, desc="Compacting tokenized corpus") if t != -1]
+                token2indices = defaultdict(dict)
+                for i, t in enumerate(tqdm(tokenized, desc="Rebuilding token indices")):
+                    if not t in special_tokens:
+                        token2indices[t][i] = None
             #tprint(tokenized, decode)
             #end(end='')
             pbar.update(1)
         #print({k: vocab[k] for k in list(vocab.keys())[orig_vocab_len:]})
         #print(extra_merges)
-        tokenizer_json = json.load(open(os.path.join(args.pretrained_model, "tokenizer.json"), "rt"))
         tokenizer_json["model"]["vocab"] = vocab
         tokenizer_json["model"]["merges"].extend(extra_merges)
         os.makedirs(args.edited_model, exist_ok=True)
